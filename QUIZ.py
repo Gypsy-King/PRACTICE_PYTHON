60388d8abb816e8105ea01319b3c5b59ea25d1b9
Gypsy-King <choikungwan7@gmail.com>
2021-11-17 18:56:00 +0900

    add Quiz 1 and the answer
    print a sentance

---
+++ QUIZ.py
@@ -0,0 +1,8 @@
+# QUIZ 1
+# Use variables to print sentances below
+
+# 1. name of variable : station
+# 2. value of variable : "Queens Bound", "Manhattan Bound", "Bronx Bound"
+# : The train for XX Bound is now approaching
+station = "Queens Bound"
+print("The train for",station,"is now approaching")
