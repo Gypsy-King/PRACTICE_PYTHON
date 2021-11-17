9409974a1f3f8c83d04384a6b178c77a13bb3437
Gypsy-King <choikungwan7@gmail.com>
2021-11-17 22:12:25 +0900

    add math funtions
    add code with abs, pow, max, min,..... and also test math library.

--- practice-2.py
+++ practice-2.py
@@ -18,3 +18,17 @@ number /= 2 # = 18
 number -= 2 # = 16
 
 number %= 5 # = 1
+
+# math functions
+
+print(abs(-5))				# 5(absolute value)
+print(pow(4, 2))			# 4^2 = 4*4 = 16(square value)
+print(max(5, 12))			# 12(maximun value between numbers)
+print(min(5, 12))			# 5(minimun value between numbers)
+print(round(3.14))			# 3
+print(round(4.99))			# 5
+
+from math import * # use all functions in math library
+print(floor(4.99)) # = 4 (round up)
+print(ceil(3.14)) # = 4 (round off)
+print(sqrt(16)) # = 4 (square root)
