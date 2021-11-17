d237d15cf39c546e6ed8bfdf996ffc09e0c49e33
Gypsy-King <choikungwan7@gmail.com>
2021-11-17 21:58:00 +0900

    add operator
    add some codes to learn usage of operators(arithmetic, boolean, bitwise, logical....)

--- practice.py
+++ practice.py
@@ -44,4 +44,30 @@ marks to commenting several sentances'''
 # commenting several 
 # sentances at once
  
- 
+# operator
+
+print(1+1) # = 2
+print(3-2) # = 1
+print(5*2) # = 10
+print(6/3) # = 2
+
+print(2**3) # = 2^3 = 8
+print(5%3) # compute remainder = 2
+print(10%3) # = 1
+print(5//3) # = 1
+print(10//3) # = 3
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+print(1 != 3) # True
+print(not(1 != 3)) # False(not(True))
+print((3 > 0) and (3 < 5))=print((3 > 0) & (3 < 5)) # True
+print((3 > 0) or (3 < 5))  = print((3 > 0) | (3 < 5)) # True
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
