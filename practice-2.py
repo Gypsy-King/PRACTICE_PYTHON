4da00ce840484a0c2b471ffc8fe7a14f4443ee33
Gypsy-King <choikungwan7@gmail.com>
2021-11-17 22:04:38 +0900

    new file
    there is too many codes in practice.py, I made new file. Added simple equations to use operators like +=, -=, /=....

---
+++ practice-2.py
@@ -0,0 +1,20 @@
+# Simple equation
+print(2 + 3 * 4) # = 14
+print((2 + 3) * 4) # = 20
+
+number = 2 + 3 * 3 # = 14
+print(number)
+
+number = number + 2 # = 16
+print(number) # = 16
+
+number += 2  # = 18
+print(number) 
+
+number *= 2 # = 36
+
+number /= 2 # = 18
+
+number -= 2 # = 16
+
+number %= 5 # = 1
