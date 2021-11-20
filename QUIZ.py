b313e8e54ff5627210252f215a4135f43c5da285
Gypsy-King <choikungwan7@gmail.com>
2021-11-20 20:04:10 +0900

    add Quiz 2
    Quiz 2 is practice for random function. Select a offline meeting date example.

--- QUIZ.py
+++ QUIZ.py
@@ -6,3 +6,20 @@
 # : The train for XX Bound is now approaching
 station = "Queens Bound"
 print("The train for",station,"is now approaching")
+
+# QUIZ 2 
+
+'''You made a new coding community recently. 
+You make a meetings four times in a month, three time is on online and rest is offline. 
+Make a program that select offline meeting date fit into evidence below.'''
+
+# 1. have to select date randomly
+# 2. consider number of day is different from month to month, maximun is 28
+# 3. exclude 1st ~ 3rd of each month during the ready for study
+
+# (printout example)
+# offline study date is selected to Xth each month.
+
+from random import *
+date = randint(4,28)
+print("offline study date is selected to" +str(date))
